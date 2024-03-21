e36fe59947369459a5057d4d29a325a6c831183a
rockplace-db1 <135800435+rockplace-db1@users.noreply.github.com>
2024-03-21 14:15:38 +0900

    Update db_info.sql

--- pg/db_info.sql
+++ pg/db_info.sql
@@ -1,19 +1,39 @@
---
+-- 
+-- 21 March 24 Updated
 -- 17 Sep 2023 Created
---
-select current_timestamp, current_database(), pg_backend_pid() ;
+-- 
+--select current_timestamp, current_database(), pg_backend_pid() ;
 
-select version() ;
+--select version() ;
 
-select d.datname, d.oid
-, pg_size_pretty(pg_database_size(d.oid)) 
-, age(d.datfrozenxid)
+select d.datname as "DB name"
+, case
+when d.size >= 1099511627776
+THEN to_char(d.size / 1099511627776.0, '999D999') || ' TB'
+WHEN d.size >= 1073741824
+THEN to_char(d.size / 1073741824.0, '999D999') || ' GB'
+WHEN d.size >= 1048576
+THEN to_char(d.size / 1048576.0, '999D999') || ' MB'
+WHEN d.size >= 1024
+THEN to_char(d.size / 1024.0, '999D999') || ' KB'
+ELSE to_char(d.size, '999') || ' byte(s)' END AS "DB size"
+, to_char(age, '9,999,999,999') AS "DB age"
 , d.dattablespace
-, pg_encoding_to_char(d.encoding)
 , d.encoding
-, d.datistemplate
-, d.datallowconn, d.datconnlimit
-from pg_database d ;
+from (
+select d.datname
+--, d.oid
+, pg_database_size(d.oid) AS "size"
+, age(d.datfrozenxid) AS "age"
+, d.dattablespace
+, pg_encoding_to_char(d.encoding) AS "encoding"
+--, d.encoding
+--, d.datistemplate
+--, d.datallowconn, d.datconnlimit
+from pg_database d
+where d.datname not in ('edb', 'postgres', 'template1', 'template0')
+and d.datistemplate <> true
+) d ;
 
 --\l+
 
