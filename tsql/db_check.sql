2dc62b57b51c4f00a4afe8159dd0efae6ccd5682
rockplace-db1 <135800435+rockplace-db1@users.noreply.github.com>
2024-05-24 16:27:02 +0900

    Delete tsql/db_check.sql

--- tsql/db_check.sql
+++
@@ -1,8 +0,0 @@
---
--- 31 Oct 2023
--- 
-SELECT 'dbcc checkdb('''+name+''') with NO_INFOMSGS '
---, PHYSICAL_ONLY
-FROM sys.databases
---WHERE name NOT IN ('tempdb', 'master', 'msdb', 'model')
-ORDER BY name ASC
