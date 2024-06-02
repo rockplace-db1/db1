e4907a8b4ea7b2cd9fb9ed7e8a06ffb1af712030
rockplace-db1 <135800435+rockplace-db1@users.noreply.github.com>
2024-06-02 22:39:37 +0900

    Add files via upload

---
+++ pg/run_grep.bash
@@ -0,0 +1,162 @@
+# 
+# 01 June 24 Created at 22:28, arithmatic expansion
+# 
+#ERR_LOG_FILES=POSFMES_202*.log
+#ERR_LOG_FILES=../231208/*.log
+ERR_LOG_FILES=../231204/edb-2023-*.log
+
+AWK_CMD=/bin/awk
+EXPR_CMD=/bin/expr
+HEAD_CMD=/bin/head
+GREP_CMD=/bin/grep
+PRINTF_CMD=/bin/printf
+SORT_CMD=/bin/sort
+TAIL_CMD=/bin/tail
+WC_CMD=/bin/wc
+
+# 
+# subroutines
+#
+searchLogForLockwaits()
+{
+  # Length must be greater than 0
+  EVENT_LOCKWAIT_ELAPSED="still waiting for"
+  N_LINES_AFTER=2
+  N_MSGS=1
+
+  $PRINTF_CMD "\n"
+
+  # double quote 
+  N_COUNT=`$GREP_CMD -h "$EVENT_LOCKWAIT_ELAPSED" $ERR_LOG_FILES | $WC_CMD -l`
+  RET_CODE=$?
+  if [ $N_COUNT -eq 0 ] ;
+  then
+    $PRINTF_CMD "No message was found: Lock wait\n"
+    return 0
+  fi
+  $PRINTF_CMD "%s message(s) found: Lock wait\n" "$N_COUNT"
+  F_MSEC=`$GREP_CMD -h "$EVENT_LOCKWAIT_ELAPSED" $ERR_LOG_FILES | $GREP_CMD -oP "after \K.+" | $AWK_CMD '{ printf "%s\n",$1 }' | $SORT_CMD -nr | $HEAD_CMD -n $N_MSGS`
+  RET_CODE=$?
+  $PRINTF_CMD "The longest wait time was %s milliseconds as follows:\n" "$F_MSEC"
+  $GREP_CMD -h -A "$N_LINES_AFTER" "$F_MSEC" $ERR_LOG_FILES 
+  RET_CODE=$?
+}
+
+searchLogForExectime()
+{
+  # Length must be greater than 0
+  EVENT_SQLEXEC_TIMETAKEN="duration:"
+  N_LINES_AFTER=1
+  N_MSGS=1
+
+  $PRINTF_CMD "\n"
+
+  # double quote 
+  N_COUNT=`$GREP_CMD -h "$EVENT_SQLEXEC_TIMETAKEN" $ERR_LOG_FILES | $WC_CMD -l`
+  RET_CODE=$?
+  if [ $N_COUNT -eq 0 ] ;
+  then
+    $PRINTF_CMD "No message was found: SQL execution time(duration)\n"
+    return 0
+  fi
+  $PRINTF_CMD "%s message(s) found: SQL exec time(duration)\n" "$N_COUNT"
+  F_MSEC=`$GREP_CMD -h "$EVENT_SQLEXEC_TIMETAKEN" $ERR_LOG_FILES | $GREP_CMD -oP "LOG:  \K.+" | $AWK_CMD '{ printf "%s\n",$2 }' | $SORT_CMD -nr | $HEAD_CMD -n $N_MSGS`
+  RET_CODE=$?
+  $PRINTF_CMD "The longest exec time was %s milliseconds as follows:\n" "$F_MSEC"
+  $GREP_CMD -h -A "$N_LINES_AFTER" "$F_MSEC" $ERR_LOG_FILES 
+  RET_CODE=$?
+}
+
+searchLogForTempfile()
+{
+  # Length must be greater than 0
+  EVENT_TEMPFILE_DELETED="temporary file:"
+  N_LINES_AFTER=1
+  N_MSGS=1
+
+  # arithmatic expansion suggested by Gemini
+  N_LINES_OUTPUT=$(($N_MSGS * ($N_LINES_AFTER + 1)))
+  #$PRINTF_CMD "DEBUG: %s\n" "$N_LINES_OUTPUT"
+
+  $PRINTF_CMD "\n"
+
+  # double quote 
+  N_COUNT=`$GREP_CMD -h "$EVENT_TEMPFILE_DELETED" $ERR_LOG_FILES | $WC_CMD -l`
+  RET_CODE=$?
+  if [ $N_COUNT -eq 0 ] ;
+  then
+    $PRINTF_CMD "No message was found: Temporary file\n"
+    return 0
+  fi
+  $PRINTF_CMD "%s message(s) found: Temp. file\n" "$N_COUNT"
+  F_MSEC=`$GREP_CMD -h "$EVENT_TEMPFILE_DELETED" $ERR_LOG_FILES | $GREP_CMD -oP "LOG:  \K.+" | $AWK_CMD '{ printf "%s\n",$6 }' | $SORT_CMD -nr | $HEAD_CMD -n $N_MSGS`
+  RET_CODE=$?
+  $PRINTF_CMD "The largest temp. file was %s bytes as follows:\n" "$F_MSEC"
+  $GREP_CMD -h -A "$N_LINES_AFTER" "$F_MSEC" $ERR_LOG_FILES | $TAIL_CMD -n $N_LINES_OUTPUT
+  RET_CODE=$?
+}
+
+countMsgBySev()
+{
+  ERR_LEVELS="PANIC FATAL WARNING ERROR"
+
+  $PRINTF_CMD "\nNumber of Messages by Msg Severities\n" 
+  for ERR_LEVEL in $ERR_LEVELS ;
+  do
+    N_COUNT=`$GREP_CMD -h "$ERR_LEVEL:" $ERR_LOG_FILES | $WC_CMD -l`
+    RET_CODE=$?
+    $PRINTF_CMD "%s  %s\n" "$ERR_LEVEL" "$N_COUNT"
+  done ;
+}
+
+printBanner()
+{
+  $PRINTF_CMD "Created only for EDB Postgres Advanced Server (EPAS) v10 or above\nand Red hat Enterprise Linux (RHEL) 7 or above\n\n"
+}
+
+searchErrForDeadlockPG()
+{
+  # Length must be greater than 0
+  EVENT_DEADLOCK_DETECTED="ERROR:  deadlock detected"
+  N_LINES_AFTER=6
+  N_MSGS=1
+
+  # arithmatic expansion suggested by Gemini
+  N_LINES_OUTPUT=$(($N_MSGS * ($N_LINES_AFTER + 1)))
+  #$PRINTF_CMD "DEBUG: %s\n" "$N_LINES_OUTPUT"
+
+  $PRINTF_CMD "\n"
+
+  # double quote 
+  N_COUNT=`$GREP_CMD -h "$EVENT_DEADLOCK_DETECTED" $ERR_LOG_FILES | $WC_CMD -l`
+  if [ $N_COUNT -eq 0 ] ;
+  then
+    $PRINTF_CMD "No message was found: Deadlock detection\n"
+    return 0
+  elif [ $N_COUNT -eq 1 ] ;
+  then
+    $PRINTF_CMD "1 message found: Deadlock detection\n"
+    $GREP_CMD -h -A "$N_LINES_AFTER" "$EVENT_DEADLOCK_DETECTED" $ERR_LOG_FILES | $HEAD_CMD -n $N_LINES_OUTPUT
+    RET_CODE=$?
+    return $RET_CODE
+  fi
+  $PRINTF_CMD "%s messages found: Deadlock detection\n" "$N_COUNT"
+  $PRINTF_CMD "The 1st and the Last deadlock detection was as follows:\n" 
+  $GREP_CMD -h -A "$N_LINES_AFTER" "$EVENT_DEADLOCK_DETECTED" $ERR_LOG_FILES | $HEAD_CMD -n $N_LINES_OUTPUT
+  RET_CODE=$?
+  $GREP_CMD -h -A "$N_LINES_AFTER" "$EVENT_DEADLOCK_DETECTED" $ERR_LOG_FILES | $TAIL_CMD -n $N_LINES_OUTPUT
+  RET_CODE=$?
+}
+
+# 
+# main
+# 
+printBanner
+$PRINTF_CMD "\nTrying to search LOG:  ...\n"
+searchLogForLockwaits
+searchLogForExectime
+searchLogForTempfile
+countMsgBySev
+$PRINTF_CMD "\nTrying to search ERROR:  ...\n"
+searchErrForDeadlockPG
+#searchErrForDeadlockEPAS
